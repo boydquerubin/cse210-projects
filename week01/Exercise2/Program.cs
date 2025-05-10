f8d2a0d974e6a78ef5dc890f4def2903991a4a1e
Boyd Querubin <boydisaacq@gmail.com>
2025-05-10 08:22:04 -0600

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,64 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90) 
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = grade % 10;
+
+        if (letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (letter == "A" && grade >= 93)
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
+
+        else
+        {
+            Console.WriteLine("Unlucky, you failed! Better luck next time.");
+        }
     }
 }
