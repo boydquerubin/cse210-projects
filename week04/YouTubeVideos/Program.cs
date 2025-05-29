2c76e0b3aab7dbd897cd8891794fa3a7082d0484
Boyd Querubin <boydisaacq@gmail.com>
2025-05-29 17:44:36 -0600

    youtube

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,32 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
-        Video video1 = new Video();
-        video1._title = "How to tie shoelaces";
-        video1._author = "Samuel Costa";
-        video1._length = 100;
+        // Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
 
-        Video video2 = new Video();
-        video2._title = "How to cook spaghetti";
-        video2._author = "Brendan Brady";
-        video2._length = 300;
+        Video video1 = new Video("How to tie shoelaces", "Samuel Costa", 100);
+        video1.AddComment(new Comment("Alice", "Great video!"));
+        video1.AddComment(new Comment("Bob", "This helped a lot!"));
+        video1.AddComment(new Comment("Charlie", "Thank you!"));
+        videos.Add(video1);
 
-        Video video3 = new Video();
-        video3._title = "How to breakdance";
-        video3._author = "Anna Thurston";
-        video3._length = 500;
+        Video video2 = new Video("How to cook Spaghetti", "Brendan Brady", 300);
+        video2.AddComment(new Comment("Dave", "This helped so much."));
+        video2.AddComment(new Comment("Frank", "Clear and easy to follow."));
+        videos.Add(video2);
 
-        Video video4 = new Video();
-        video4._title = "How to cook chicken";
-        video4._author = "Ugochukwu Febechukwu";
-        video4._length = 200;
+        Video video3 = new Video("How to Breakdance", "Anna Thurston", 500);
+        video3.AddComment(new Comment("Grace", "I was looking for a video like this!"));
+        videos.Add(video3);
 
-        Comment comment1 = new Comment();
-        comment1._comment = "this video is great!";
-        comment1._comment.Add(video1);
+        Video video4 = new Video("How to cook Chicken", "Ugochukwu Febechukwu", 200);
+        video4.AddComment(new Comment("Jack", "This video is great!"));
+        video4.AddComment(new Comment("Jill", "So good!"));
+        videos.Add(video4);
+
+        foreach (Video video in videos)
+        {
+            video.DisplayInfo();
+        }
     }
 }
